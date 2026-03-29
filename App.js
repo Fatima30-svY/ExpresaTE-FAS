377e52b6117cea6ae61edfe5c79844a38a0858be
Fatima Sanchez <L202307060@vbravo.tecnm.mx>
2026-03-28 22:15:08 -0600

    Corrige duplicado Finalizar en App.js

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import RegistrarCuenta from './Screens/RegistrarCuenta';
 import PantallaPrincipal from './Screens/PantallaPrincipal.js';
 import Finalizar from './Screens/UltimaPantalla.js';
 import SeccionScreen from './Screens/SeccionScreen';
-
 import Perfil from './Screens/Perfil.js';
 
 const Stack = createNativeStackNavigator();
@@ -20,14 +19,13 @@ export default function App() {
         initialRouteName="Login"
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Finalizar" component={Finalizar} />
+        <Stack.Screen name="UltimaPantalla" component={Finalizar} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="RegistrarCuenta" component={RegistrarCuenta} />
         <Stack.Screen name="PantallaPrincipal" component={PantallaPrincipal} />
         <Stack.Screen name="Seccion1" component={SeccionScreen} />
         <Stack.Screen name="Seccion2" component={SeccionScreen} />
         <Stack.Screen name="Seccion3" component={SeccionScreen} />
-        <Stack.Screen name="Finalizar" component={Finalizar} />
         <Stack.Screen name="Perfil" component={Perfil} />
       </Stack.Navigator>
     </NavigationContainer>
